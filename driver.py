ec24c558c7981630f7b1579766e080725396b343
donox <don@theoxleys.co>
2022-04-10 06:08:34 -0400

    Consolidate to single logger

--- driver.py
+++ driver.py
@@ -33,34 +33,6 @@ def driver():
     except:
         raise SystemError("No USER Environment Variable specified")
 
-    # NOTE:  Variables are now set in config_private to allow them to be changed by the user without
-    #        causing clashes when updating code from github.
-    # # This script is intended to run daily, so there is a notion of 'testing' which is applicable
-    # # during development or other non-automated execution.  Automated execution requires creating
-    # # a Linux timer driven script.
-    # do_testing = True
-    # # Assume that any run between 1am and 4am is for production
-    # start_notest = dt.time(1, 0)  # but not if between 1am and 4am
-    # end_notest = dt.time(3, 0)
-    # if start_notest < dt.datetime.now().time() < end_notest:
-    #     do_testing = True
-    #
-    # if do_testing:
-    #     prototyping = False
-    #     sst_management = True
-    #     build_user_list = False
-    #     build_staff_list = False
-    #     build_horizon_list = False
-    #     create_combined_login = False
-    #
-    # else:           # Intended for cron job run nightly
-    #     prototyping = False
-    #     sst_management = False
-    #     build_user_list = False
-    #     build_staff_list = False
-    #     build_horizon_list = False
-    #     create_combined_login = False
-
     config = configparser.ConfigParser()
 
     # Load parameters from configuration file
@@ -80,14 +52,16 @@ def driver():
     email_username = pvt.username
     email_password = pvt.password
 
-    summary_logger = OvernightLogger('summary_log', logs_directory)  # Logger - see use below
+    summary_logger = OvernightLogger('sst_utils_log', logs_directory)  # Logger - see use below
     summary_logger.make_info_entry(f"Begin sst_utils run ")
 
     if pvt.sst_management:
         summary_logger.make_info_entry(f"Begin command processing")
-        # commands.txt user prefix set with: os.environ["USER_PREFIX"]
+        # commands.txt user prefix set with: os.environ["USER_PREFIX"] of the form "username_"
+        # where username must exist in config_users.yaml
         sys_mgr = sm.SystemManager()
         sys_mgr.run_command_processor()
+        summary_logger.make_info_entry(f"Complete command processing")
 
     if pvt.build_user_list or pvt.build_staff_list or pvt.build_horizon_list:
         summary_logger.make_info_entry(f"Begin login list processing")
@@ -125,11 +99,10 @@ def driver():
         # Copy current login lists from Google Drive, combine them and move to sst/support/users.csv
         try:
             summary_logger.make_info_entry(f"Begin create combined login")
-            sst_logger = OvernightLogger('build_users_csv', logs_directory)
-            sst_logger.make_info_entry('Start User Login Creation')
 
             resident_phone_list = "Sunnyside Resident Phone Directory. 01-2022.xls"
             staff_phone_list = "Sunnyside Staff Directory. 01-2022.xls"
+            # TODO: move directory to config file, check if this is right way to handle temps
             google_drive_dir = "SSTmanagement/UserData/"
             temps = temp_directory + 'user_list_temp/'
             if os.path.exists(temps):  # Anything from prior runs is gone
@@ -138,33 +111,29 @@ def driver():
 
             files = ['residents.csv', 'staff.csv', 'horizon.csv']
             outfile = 'users.csv'
-            res_list_processor = CreateUserList(sst_logger, temps, google_drive_dir)
+            res_list_processor = CreateUserList(summary_logger, temps, google_drive_dir)
             res_list_processor.get_all_users(files, outfile)
 
             shutil.copy(temps + 'users.csv', sst_support_directory + 'users.csv')
 
             # Log completion
-            sst_logger.make_info_entry('Complete Creation of sst/support/users.csv')
-            sst_logger.close_logger()
-            summary_logger.make_info_entry('build_users_csv completed normally')
+            summary_logger.make_info_entry('Complete Creation of sst/support/users.csv')
         except Exception as e:
             summary_logger.make_error_entry('build_users_csv failed with exception: {}'.format(e.args))
 
     if pvt.prototyping:
         """Download and unzip a backup file."""
         summary_logger.make_info_entry(f"Begin prototyping run")
-        logger = OvernightLogger('prototyping', logs_directory)
         target_directory = temp_directory + 'worktemp/'
         manage_drive = mgd.ManageGoogleDrive()
         source_dir = "SSTmanagement/Backups/"
         file_to_download = "galleries-mar-27-2022.zip"
 
-        manage_drive.download_file(logger, source_dir, file_to_download, target_directory)
+        manage_drive.download_file(summary_logger, source_dir, file_to_download, target_directory)
         foo = 3
 
     if False and pvt.prototyping:
         """Modify meta_paths in directory containing possibly recursive structure of files. """
-        logger = OvernightLogger('prototyping', logs_directory)
         drive_dir_to_download = config['drive paths']['driveAdmin'] + config['drive paths']['driveMinutes']
         target_directory = work_directory + 'worktemp/'
         test_dir = temp_directory + 'fix_meta_paths/'
@@ -203,183 +172,9 @@ def driver():
                     except Exception as e:
                         foo = 4
 
-            logger.close_logger()
-
         except Exception as e:
             print(e)
             traceback.print_exc()
-
-    if False and pvt.prototyping:
-        """Test validate shortcodes."""
-        logger = OvernightLogger('prototyping', logs_directory)
-        db_name = 'sst'
-        drive_dir_to_download = config['drive paths']['driveAdmin'] + config['drive paths']['driveMinutes']
-        target_directory = work_directory + 'worktemp/'
-        test_dir = temp_directory + 'validate/'
-        try:
-            filepath = test_dir + 'Springtime.docx'
-            filetype = 'docx'
-            val_sc = vs.ValidateShortcodes(filepath, filetype, logger)
-            val_sc.clean_docx()
-            val_sc.process_shortcodes()
-            logger.close_logger()
-
-        except Exception as e:
-            print(e)
-            traceback.print_exc()
-
-    if False and pvt.prototyping:
-        """Provide support for flexbox."""
-        logger = OvernightLogger('prototyping', logs_directory)
-        db_name = 'sst'
-        drive_dir_to_download = config['drive paths']['driveAdmin'] + config['drive paths']['driveMinutes']
-        target_directory = work_directory + 'worktemp/'
-        try:
-            from collections import deque
-
-            class SupportFlexbox(object):
-                """Provide support for css flexbox.
-
-                Nikola does not support nested shortcodes so we remove the "box" shortcode and
-                convert it to proper html on the markdown file prior to submitting to nikola."""
-                # Box start shortcode:  {{% box name="xxx" direction="row" %}}
-                box_start = r'(?P<start>\{\{% +box\s+name="(?P<name_s>\w+)"'
-                box_start += r'(\s+(?P<attrs>((((\w+)="((\w|:|-)+)")\s+))*))%\}\})'
-                box_end = r'(?P<end>\{\{% +/box\s+name="(?P<name_e>\w+)"\s+%\}(?P<last_char>\}))'
-                # Note: DOTALL makes '.' include newlines
-                box_comp_start = re.compile(box_start)
-                box_comp_end = re.compile(box_end)
-                box_attr = re.compile(r'(?P<attribute>\w+)="(?P<value>(\w|_|-|:|;)+)"')
-
-                def __init__(self):
-                    self.stack = deque()
-
-                def process_box_shortcodes(self, in_string, result):
-                    """Process next shortcode and recurse to process nested codes.
-
-                    There are three cases:
-                    (1) input does not match start  or end of shortcode:  return in_string
-                    (2) input finds first open box:
-                            (a) get box name, other parameters and push on stack.
-                            (b) recurse on tail of in_string.
-                    (3) input finds close box:
-                        (a) get name and compare to top of stack - must match or throw error.
-                        (b) emit html using parameters from stack and surround current in_string as returned
-                            from recursion.
-                        (c) process tail."""
-                    if not in_string:
-                        return ''
-
-                    # Determine closest box shortcode and amount of in_string before it
-                    start_pos_start = start_pos_end = end_pos_start = end_pos_end = None
-                    search_start = self.box_comp_start.search(in_string)
-                    if search_start:
-                        start_pos_start, start_pos_end = search_start.span()
-                    search_end = self.box_comp_end.search(in_string)
-                    if search_end:
-                        end_pos_start, end_pos_end = search_end.span()
-                    # Determine beginning of closest box shortcode
-                    if search_start and start_pos_end:
-                        str_beginning = min(start_pos_start, end_pos_start)
-                    elif search_start:
-                        str_beginning = start_pos_start
-                    elif search_end:
-                        str_beginning = end_pos_start
-                    else:
-                        str_beginning = len(in_string)
-
-                    if str_beginning > 0:
-                        result.append(in_string[0:str_beginning])
-                        self.process_box_shortcodes(in_string[str_beginning:], result)
-                    elif str_beginning == start_pos_start:
-                        # Process box beginning
-                        box_match = search_start.groupdict()
-                        box_match_keys = box_match.keys()
-                        if 'name_s' in box_match_keys:
-                            print(f"Matched start: {box_match['name_s']}")
-                            self.stack.append(box_match)
-                            print(f"PUSH: {self.stack[-1]['name_s']}")
-                            a_tmp = box_match['attrs']
-                            begin_text = self._build_flex_container_start(box_match['attrs'])
-                            result.append(begin_text)
-                            # print(f"1: {in_string[match_len:]}")
-                            self.process_box_shortcodes(in_string[start_pos_end:], result)
-                            foo = 3
-                    elif str_beginning == end_pos_start:
-                        # process box end
-                        box_match = search_end.groupdict()
-                        box_match_keys = box_match.keys()
-                        if 'name_e' in box_match_keys:
-                            end_name = box_match['name_e']
-                            print(f"Matched end: {box_match['name_e']}")
-                            if not self.stack:
-                                raise ValueError(f'Closing box found with no matching start')
-                            start_dict = self.stack[-1]
-                            start_name = start_dict['name_s']
-                            if start_name != end_name:
-                                raise ValueError(f'Unmatched containers: {start_name} and {end_name}')
-                            print(f"POP: {self.stack[-1]['name_s']}")
-                            self.stack.pop()
-                            result.append('</div>')
-                            self.process_box_shortcodes(in_string[end_pos_end:], result)
-                            foo = 3
-                    else:
-                        result.append(in_string)
-
-                def _build_flex_container_start(self, attrs):
-                    attr_dict = dict()
-                    while True:
-                        parsed_attrs = self.box_attr.search(attrs)
-                        if not parsed_attrs:
-                            break
-                        attr_dict[parsed_attrs.group('attribute')] = parsed_attrs.group('value')
-                        attrs = attrs[parsed_attrs.end('value'):]
-                    dict_keys = list(attr_dict.keys())
-                    if 'direction' in dict_keys:
-                        dict_keys.remove('direction')
-                        val = attr_dict['direction']
-                        if val == 'row':
-                            cls = 'src_flex_container'
-                        elif val == 'row-reverse':
-                            cls = 'src_flex_container_rev'
-                        elif val == 'column' or val == 'col':
-                            cls = 'src_flex_container_col'
-                        elif val == 'column-reverse' or val == 'col-reverse':
-                            cls = 'src_flex_container'
-                        else:
-                            cls = f'src_flex_container UNRECOGNIZED BOX DIRECTION {val}'
-                    other_attrs = []
-                    for key in dict_keys:
-                        other_attrs.append(f' {key}="{attr_dict[key]}"')
-                    other_attrs = ' '.join(other_attrs)
-                    start_str = f'<div class="{cls}" {other_attrs}>'
-                    return start_str
-
-            pbs = SupportFlexbox()
-            a_test_string = ''
-            # a_test_string += 'Now is the time '
-            # a_test_string += ' {{% box   name="foo" \n direction="row" %}}'
-            # a_test_string += ' {{% cvb   name="foo"  direction="row" %}}'
-            # a_test_string += ' {{% box \n  name="bar"  direction="column" %}}'
-            # a_test_string += ' xx random stuff xx'
-            # a_test_string += ' {{% /box name="bar" %}}'
-            # # a_test_string += r'    direction="column"   style="display:None"   '
-            # a_test_string += ' {{% box   name="baz"  direction="column" style="display:None" %}}'
-            # a_test_string += ' xx random stuff xx'
-            # a_test_string += ' {{% /box name="baz" %}}'
-            # a_test_string += ' xx boxitems xx'
-            # a_test_string += ' {{% /box name="foo" %}}'
-            # a_test_string += '  asdfasdf asf'
-            res = []
-            pbs.process_box_shortcodes(a_test_string, res)
-            full_result = "".join(res)
-
-            foo = 3
-        except Exception as e:
-            print(e)
-            traceback.print_exc()
-
-        logger.close_logger()
 
     summary_logger.make_info_entry('sst_utils Run Completed')
     summary_logger.close_logger()
